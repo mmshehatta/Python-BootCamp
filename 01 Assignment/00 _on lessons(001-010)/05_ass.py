12b9e402e3b09b3e6217b8c63be2c323deea7f38
Mahmoud <mmshehatta@gmail.com>
2021-07-07 17:33:54 +0200

    adding lessons and assignments sections

---
+++ 01 Assignment/00 _on lessons(001-010)/05_ass.py
@@ -0,0 +1,17 @@
+#  قم بطباعة نوع كل متغير من المتغيرات التي أنشأناها في سطر منفصل ليظهر لنا ال Output التالي 
+
+
+age = '25'
+name ="mahmoud"
+country = """Egypt"""
+
+print("===== ass5 ======")
+print(type(name))
+print(type(age))
+print(type(country))
+
+# result:
+# ===== ass5 ======
+# <class 'str'>
+# <class 'str'>
+# <class 'str'>
