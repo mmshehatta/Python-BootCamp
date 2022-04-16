45e325f4891132ab44f142b9fcefa01e266d078f
Mahmoud Shehata <mmshehatta@gmail.com>
2022-04-16 22:55:41 +0200

    week#2::Commit2:: Selving Ass1 & Ass2

---
+++ week_2/01_assignments/Answers/ass2.py
@@ -0,0 +1,15 @@
+"""
+التكليف 02
+-----------
+قم بطباعة نفس الرسالة السابقة كما هي ولكن على أكثر من سطر, شاهد ال Output المطلوب
+
+"""
+name = "Mahmoud"
+age = 26
+country = "Egypt"
+
+print(f"""
+    "Hello '{name}' , How are you doing \\
+     \"\"\" Your Age Is \"{age}\"\" +
+     And Your Country Is: {country}
+""")
