9b9a2952239a4e3741c6c8d64c4f356287919f95
Mahmoud Shehata <mmshehatta@gmail.com>
2022-04-16 23:17:22 +0200

    week#2::Commit2:: Solving Ass3

---
+++ week_2/01_assignments/Answers/ass3.py
@@ -0,0 +1,8 @@
+
+name = "ElZero"
+print("Second Letter : {} ".format(name[1]))  # first Method.
+print(f"Second Letter : {name[1]}")  # second Method.
+
+print("Third Letter : {}".format(name[2]))
+
+print("Last Letter : {} ".format(name[-1]))
