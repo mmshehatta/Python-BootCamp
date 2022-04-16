dfe6dfbb82e53cee27eeb4c02a2d5ab8be0601bb
Mahmoud Shehata <mmshehatta@gmail.com>
2022-04-16 22:55:41 +0200

    week#2::Commit2:: Selving Ass1 & Ass2

--- week_2/01_assignments/Answers/ass1.py
+++ week_2/01_assignments/Answers/ass1.py
@@ -1,3 +1,11 @@
 """ 
  قم بإنشاء ثلاث متغيرات عبارة عن اسمك وسنك وبلدك ثم قم بطباعة الرسالة التالية مع عمل Concatenate للمتغيرات مع العلامات والكلمات التالية لتحصل على نفس الرسالة في النهاية, يجب عليك إظهار الرسالة كما هي بجميع العلامات الموجودة بنفس الترتيب والمسافات
 """
+from icecream import ic
+# "Hello 'Osama', How You Doing \ """ Your Age Is "38"" + And Your Country Is: Egypt
+name = "Mahmoud"
+age = 26
+country = "Egypt"
+
+print(f"Hello '{name}' , How are you Doing \ \"\"\" Your Age Is \"{age}\"\" " +
+      "And Your Country Is: {country}")
