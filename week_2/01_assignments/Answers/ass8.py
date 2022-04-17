ab00b2020c13a98e08a1d887c2d988dba6b778b5
Mahmoud Shehata <mmshehatta@gmail.com>
2022-04-17 05:09:55 +0200

    week#2::Commit7:: Solving Ass8

---
+++ week_2/01_assignments/Answers/ass8.py
@@ -0,0 +1,17 @@
+# Variables
+name_one = "OSamA"
+name_two = "osaMA"
+# Processing
+
+
+def convert_str(str):
+    return str.swapcase()
+
+
+print(convert_str(name_one))
+print(convert_str(name_two))
+
+
+# output
+# osAMa
+# OSAma
