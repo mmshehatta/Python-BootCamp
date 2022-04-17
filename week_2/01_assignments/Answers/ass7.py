fe2b32bcc5fdf5e311516678f3a4047252765050
Mahmoud Shehata <mmshehatta@gmail.com>
2022-04-17 03:59:19 +0200

    week#2::Commit6:: Solving Ass7

---
+++ week_2/01_assignments/Answers/ass7.py
@@ -0,0 +1,16 @@
+# Variables
+
+# Processing
+
+
+def add_spcial_char(str):
+    return str.rjust(20, "@")
+
+
+print(add_spcial_char("Osama"))
+print(add_spcial_char("Osama_ElZero"))
+
+
+# output
+# @@@@@@@@@@@@@@@Osama
+# @@@@@@@@Osama_ElZero
