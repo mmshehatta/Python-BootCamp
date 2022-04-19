eefcd069afac8d1e0805ee6345f65a50d260a55e
Mahmoud Shehata <mmshehatta@gmail.com>
2022-04-19 02:13:31 +0200

    week#2::Commit12:: Solving Ass13

---
+++ week_2/01_assignments/Answers/ass13.py
@@ -0,0 +1,10 @@
+# Variables
+name = "Osama"
+age = 38
+country = "Egypt"
+# Processing
+print(f"My Name Is {name}, And My Age Is {age}, And My Country Is {country}")
+
+# output
+# Needed Output Using f""
+# My Name Is Osama, And My Age Is 38, And My Country Is Egypt
