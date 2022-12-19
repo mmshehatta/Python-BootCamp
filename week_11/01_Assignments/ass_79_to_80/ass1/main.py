4a60f6488985e404d30a51ab0e50b4aa0de54733
Mahmoud Shehata <mmshehatta@gmail.com>
2022-12-19 03:24:54 +0200

    `week_11_(079-080)_Solving` Ass1.

---
+++ week_11/01_Assignments/ass_79_to_80/ass1/main.py
@@ -0,0 +1,15 @@
+
+import datetime
+
+last_date = datetime.datetime(2021 , 6 , 25)
+recent_date = datetime.datetime(2021 ,8 , 10)
+
+print(f"Days From 2021-06-25 To 2021-08-10 Is => {(recent_date - last_date).days}")
+
+# output
+
+# The Date Is "2021, 6, 25"
+# Today Is "2021, 8, 10"
+
+# Message Will Be
+"Days From 2021-06-25 To 2021-08-10 Is => 46"
